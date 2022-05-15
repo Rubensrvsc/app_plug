d296e02bde841ed32ff2e2b7ff014f5fba57a786
rubensrvsc <rubens.rvsc@gmail.com>
2022-05-15 18:38:30 -0300

    refactor: configure mode like app is generate

--- lib/app_plug/application.ex
+++ lib/app_plug/application.ex
@@ -1,20 +1,25 @@
 defmodule AppPlug.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
+  
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
+    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: AppPlug.Worker.start_link(arg)
-      # {AppPlug.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: AppPlug.AppPlug, options: [port: 4001]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: AppPlug.Supervisor]
+
+    Logger.info("Starting application...")
+
     Supervisor.start_link(children, opts)
   end
 end
